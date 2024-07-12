eb453870f6e3b6e8192f6a91bf544166d4cfd193
Alyona Gepting <abriconi@gmail.com>
2024-07-12 13:51:29 +0300

    feat(app): created components AbouyMe  & Navigation

---
+++ src/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,8 @@
+import { HeadingDetailed } from "../shared-components/HeadingDetailed/HeadingDetailed";
+import styles from "./styles.module.scss";
+
+export const AboutMe = () => (
+    <div>
+        <HeadingDetailed text="About Me"/>
+    </div>
+)
