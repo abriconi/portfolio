9d7bd8b73f4992fbfa7bc7dd0d061e3236709699
Alyona Gepting <abriconi@gmail.com>
2024-07-19 16:27:21 +0300

    feat(vertical-devider): created VerticalDevider component

---
+++ src/components/shared-components/VerticalDivider/VerticalDivider.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+import styles from "./styles.module.scss";
+
+interface Props {
+    children: ReactNode;
+}
+export const VerticalDivider: React.FC<Props> = ({children}) => {
+    return (<div className={styles.divider}>{children}</div>)
+}
