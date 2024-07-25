0f23240981b3b943929a606137bdc286e23c31c1
Alyona Gepting <abriconi@gmail.com>
2024-07-25 11:37:54 +0300

    feat(scale-card): created component ScaleCard

---
+++ src/components/shared-components/ScaleCard/ScaleCard.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+import styles from "./styles.module.scss";
+
+interface Props {
+    children: ReactNode;
+}
+export const ScaleCard: React.FC<Props> = ({ children }) => {
+    return <div className={styles.scaleCard}>{children}</div>;
+};
