ecfd9d6a0b2d264459d06f4249a5205d10ff4bf0
Alyona Gepting <abriconi@gmail.com>
2024-07-29 10:23:17 +0300

    feat(naviagation): highlighted active link

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styles from "./styles.module.scss";
 import { AppRoutes } from "../../routes";
 
@@ -6,10 +6,38 @@ export const Navigation = () => {
   return (
     <nav className={styles.wrapper}>
       <ul className={styles.menu}>
-        <li className={styles.menuItem}><Link to={AppRoutes.About} className={styles.link}>About</Link></li>
-        <li className={styles.menuItem}><Link to={AppRoutes.Resume} className={styles.link}>Resume</Link></li>
-        <li className={styles.menuItem}><Link to={AppRoutes.Portfolio} className={styles.link}>Portfolio</Link></li>
-        <li className={styles.menuItem}><Link to={AppRoutes.Features} className={styles.link}>Features</Link></li>
+        <li className={styles.menuItem}>
+          <NavLink
+            to={AppRoutes.About}
+            className={({ isActive }) => isActive ? `${styles.link} ${styles.activeLink}` : styles.link}
+          >
+            About
+          </NavLink>
+        </li>
+        <li className={styles.menuItem}>
+          <NavLink
+            to={AppRoutes.Resume}
+            className={({ isActive }) => isActive ? `${styles.link} ${styles.activeLink}` : styles.link}
+          >
+            Resume
+          </NavLink>
+        </li>
+        <li className={styles.menuItem}>
+          <NavLink
+            to={AppRoutes.Portfolio}
+            className={({ isActive }) => isActive ? `${styles.link} ${styles.activeLink}` : styles.link}
+          >
+            Portfolio
+          </NavLink>
+        </li>
+        <li className={styles.menuItem}>
+          <NavLink
+            to={AppRoutes.Features}
+            className={({ isActive }) => isActive ? `${styles.link} ${styles.activeLink}` : styles.link}
+          >
+            Features
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
